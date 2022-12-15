f92c01c37ae54ce5f7dabcd126d0d46f69d08863
Аникина Лилия <zolina.liliya@gmail.com>
2022-12-15 13:16:25 +0300

    added fixies

--- source/js/modules/play-video.js
+++ source/js/modules/play-video.js
@@ -1,6 +1,6 @@
 function setupVideo() {
   let video = document.querySelector('.gym-video');
-  let link = video.querySelector('.video-link');
+  let link = video.querySelector('.gym-video__link');
   let button = video.querySelector('.gym-button');
 
   video.addEventListener('click', () => {
@@ -12,17 +12,17 @@ function setupVideo() {
   });
 
   link.removeAttribute('href');
-  video.classList.add('video--enabled');
+  video.classList.add('gym-video--enabled');
 }
 
 function createIframe() {
   let iframe = document.createElement('iframe');
 
   iframe.setAttribute('allowfullscreen', '');
+  iframe.setAttribute('allow', 'autoplay');
   iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?rel=0&showinfo=0&autoplay=1');
-  iframe.setAttribute('rel', '0&showinfo=0&autoplay=1');
   iframe.setAttribute('frameborder', '0');
-  iframe.classList.add('video', '.gym-video-player');
+  iframe.classList.add('gym-video__media');
 
   return iframe;
 }
