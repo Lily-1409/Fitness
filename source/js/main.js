d151f7823d1af51cf34855d625a008213b2d879c
Аникина Лилия <zolina.liliya@gmail.com>
2022-12-16 19:10:40 +0300

    added fixies

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import './modules/scroll';
 import './modules/play-video';
+import './modules/tabs';
 
 // ---------------------------------
 
