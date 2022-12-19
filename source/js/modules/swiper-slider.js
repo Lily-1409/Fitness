0a891333473fda06b10a7b4b24dd63feee2b5a4c
Аникина Лилия <zolina.liliya@gmail.com>
2022-12-19 18:24:03 +0300

    added fixies

--- source/js/modules/swiper-slider.js
+++ source/js/modules/swiper-slider.js
@@ -1,4 +1,4 @@
-export const swiper = new window.Swiper('.swiper', {
+export const swiperTrainers = new window.Swiper('.swiper-trainers', {
   watchoverflow: true,
   slidesPerColumn: 1,
   breakpoints: {
@@ -17,6 +17,20 @@ export const swiper = new window.Swiper('.swiper', {
   },
   // Optional parameters
   loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+export const swiperReviews = new window.Swiper('.swiper-reviews', {
+  watchoverflow: true,
+  slidesPerColumn: 1,
+  slidesPerView: 1,
+  // Optional parameters
+  loop: false,
 
   // Navigation arrows
   navigation: {
